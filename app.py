b5e96a940ef7f121def58022b3da3414df478cf1
wasid12 <wasidkhan375375@gmail.com>
2025-11-28 12:10:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import requests
 import os
 
 st.set_page_config(
-    page_title="FB E2EE by HASSAN RAJPUT",
+    page_title="FB E2EE by AFFAN MARK",
     page_icon="👑",
     layout="wide",
     initial_sidebar_state="expanded"
